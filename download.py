110015a9d9f068185411c907f0d1f2f0e0b241e8
johnlin <john830110@gmail.com>
2022-10-15 23:26:06 +0800

    feat: 新增下載英雄圖片功能

---
+++ download.py
@@ -0,0 +1,29 @@
+import requests
+import pandas as pd
+from pathlib import Path
+
+
+def download(path, name):
+    url = f"https://lol-skin.weblog.vc/img/champion/{name}.png"
+    response = requests.get(url)
+    if response.status_code == 200:
+        print(200)
+        with open(f"{path}/{name}.png", 'wb') as f:
+            f.write(response.content)
+
+
+if __name__ == '__main__':
+    # get file paths
+    path = Path('/').joinpath(Path().resolve(), 'champion')
+
+    # champion list
+    df = pd.read_csv('champion_list.csv')
+    champion_list = list(df["英文名稱"])
+    # print(champion_list)
+    # print(len(champion_list))
+
+    count = 0
+    for champion in champion_list:
+        download(path, champion)
+        print(f'{count}. {champion} OK')
+        count += 1
